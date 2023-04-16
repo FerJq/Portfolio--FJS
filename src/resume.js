b57caabe513b466ae1b4340fd1c8b538cd84c1f4
Fernanda Santiago <126302626+FerJq@users.noreply.github.com>
2023-04-15 23:47:57 -0400

    galery

--- src/resume.js
+++
@@ -1,8 +0,0 @@
-function printResume() {
-  let resumework = document.write(
-    document.getElementById("resumeImg").innerHTML
-  );
-  resumework.print();
-}
-let handleresume = document.querySelector("#printresume");
-handleresume.addEventListener("click", printResume);
